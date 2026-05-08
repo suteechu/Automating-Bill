8618423f1f924e85fc795c598f8011f3609336fd
Your Name <you@example.com>
2026-05-09 06:59:53 +0700

    Auto-update: 09-May-26  6:59:53.64

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-<<<<<<< HEAD
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/Automating-Bill/',
 })
-=======
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
->>>>>>> 30d7bcc64e48810e8a9aa265651dd8e38e1d4ce7
